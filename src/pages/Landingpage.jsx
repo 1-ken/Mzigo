8e0ddaf2b1a3c6c5a436e694b6d3addd1f517ee5
1-ken <kennicmwimu@gmial.com>
2025-09-05 20:12:04 +0300

    donw

--- src/pages/Landingpage.jsx
+++ src/pages/Landingpage.jsx
@@ -1,44 +1,54 @@
 import React from "react";
 import CompanyCard from '../components/CompanyCard'
 import logo from './lopha-travel-ltd.jpg'
+import Kasese from './kasese logo.jpeg'
+import Chania from './Chania logo.jpeg'
+import Kangema from './Kangema.jpeg'
+import Ungwana from './ungwana logo.jpeg'
+import Metro from './metro trans.jpeg'
+
+
+
+
+
 
 
 const companies = [
   {
     id: 1,
-    name: "Company Name",
+    name: "Lopha travelers ltd",
     image: logo,
-    description: "we deliver all places in nairobi",
+    description: "we deliver all places in nairobi,ruiru, thika",
   },
   {
     id: 2,
-    name: "Company Name",
-    image: logo,
-    description: "we deliver all places in nairobi",
+    name: "Kasese",
+    image: Kasese,
+    description: "we deliver from nairobi to naivasha",
   },
   {
     id: 3,
-    name: "Company Name",
-    image: logo,
+    name: "Chania",
+    image: Chania,
     description: "we deliver all places in nairobi",
   },
   {
     id: 4,
-    name: "Company Name",
-    image: logo,
-    description: "we deliver all places in nairobi",
+    name: "Kangema",
+    image: Kangema,
+    description: "we deliver all places in nairobi, murang'a and kangema",
   },
   {
     id: 5,
-    name: "Company Name",
-    image: logo,
-    description: "we deliver all places in nairobi",
+    name: "Ungwana",
+    image: Ungwana,
+    description: "we deliver all places in nairobi, meru,chuka and chogoria",
   },
   {
     id: 6,
-    name: "Company Name",
-    image: logo,
-    description: "we deliver all places in nairobi",
+    name: "Metro Trans",
+    image: Metro,
+    description: "we deliver all places in nairobi,ngong-road ngong ",
   },
 ];
 
